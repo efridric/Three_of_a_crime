cec801df7b09ac7c575f14579f72fe9258706ae5
efridric <efridric@uncc.edu>
2013-10-16 20:36:45 -0400

    intialized project

---
+++ src/GameLogic.scala
@@ -0,0 +1,9 @@
+object GameLogic {
+	//TODO: Define a method for starting a round
+  
+    //TODO: Define a method for handling player guesses
+  
+    //TODO: Define a method for losing
+  
+	//TODO: Define a method for winning
+}
---
+++ src/Main.scala
@@ -0,0 +1,11 @@
+/**
+ *  Three of a crime is a simple logic game for up to 3 players. There are 7 different criminals.
+ *  The computer will randomly choose three of these 7 criminals to be the perpetrators.
+ *  At the start of each round the computer will provide the names of 3 criminals and state how many of those 3 if any are perpetrators.
+ *  It is the job of the player to guess which 3 of the 7 criminals are the perpetrators, be careful though because if you guess wrong you are out. 
+ * 
+ */
+class Main {
+	//TODO: Initialize the players
+    //TODO: Start the game
+}
